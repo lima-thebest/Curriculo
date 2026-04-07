04e738cd8e86f00df298bfa72e84727db6693273
E o lima <gustavo.martins.lima@escola.pr.gov.br>
2026-04-07 11:34:43 -0300

    Projeto versão 1

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import fotoPerfil from './WhatsApp Image 2026-03-21 at 21.07.12.jpeg'
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1></h1>
+        <img src={fotoPerfil}></img>
+        <h2>Gustavo Emanuel=</h2>
+      </header>
+
+      <main>
+        <section>
+          <h1>Dados para contato</h1>
+          <p>Rafael Florindo</p>
+          <p>44 98431-3140</p>
+          <p>Email: gustavo.martins.lima@escola.pr.gov.br</p>
+          <p>Linkdin: xxxxxxx</p>
+          <p>Github: xxxx</p>
+        </section>
+        <section>
+          <h1>Formação</h1>
+          <div>
+            <article>
+              <h2>Ensino Medio Profissionalizante em Desenvolvimento de Sistemas</h2>
+              <p>Instituição:Colegio Santa Maria Goretti</p>
+              <p>Ano de Conclusão: 2027</p>
+            </article>
+
+            <article>
+              <h2></h2>
+              <p>Instituição</p>
+              <p>Ano de Conclusão:</p>
+            </article>
+
+          </div>
+        </section>
+        <section>
+
+        </section>
+      </main>
+
+      </div>
+      );
+}
+      export default App;
